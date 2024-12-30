3ec0671832577911220f491882d2d32f55700e88
Khantsi7920 <59691173+Khantsi7920@users.noreply.github.com>
2024-12-30 13:38:19 +0630

    初期コミット
    1週目のラーニング

---
+++ index.js
@@ -0,0 +1,38 @@
+//1. Introduction to JavaScript
+//文字形
+console.log("Hello World");
+
+//2.Data Types and Intro To Variables
+//数字形
+console.log(12.3);
+
+//Boolean形
+console.log(true);
+console.log(false);
+
+//assigning variable 
+aung_aung_name="Aung Aung";
+console.log(aung_aung_name);
+
+//undefined
+var a;
+console.log(a);
+
+//null
+var a = null;
+console.log(a);
+
+//3 Variable Format & ES 6 Variable Syntaz
+//数字の計算
+var num1=12;
+var num2=13;
+console.log(num1+num2);
+
+//数字と文字の計算
+let name = "カンシート";
+let age = 12;
+let isSingle = true;
+console.log(name+"平成" + age + " は独身？ " + isSingle)
+
+const b = 12;
+b=13//行26で固定値設定されますのでここにエラーが発生されます。
